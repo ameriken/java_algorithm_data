eb92e468f0ff297af58f9477b05388af4e9781fb
ameriken <amemiyakenji@AMEMIYAnoMacBook-Pro.local>
2020-04-20 18:39:36 +0900

    ボイヤームーア法

---
+++ src/boyermoore/Main.java
@@ -0,0 +1,12 @@
+package boyermoore;
+
+
+public class Main {
+
+    public static void main(String args[]) {
+
+        BoyerMoore bm = new BoyerMoore();
+        bm.search("AOIJOIJHOIJO", "JO");
+
+    }
+}
